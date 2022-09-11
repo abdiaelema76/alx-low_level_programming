362885d8b2a5722be370aa73dcaf3a28b1e2b144
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-11 08:24:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - prints all single base 10 numbers
+*
+*Return: 0
+*
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	putchar(num + '0');
+
+	putchar('\n');
+	return (0);
+	}
