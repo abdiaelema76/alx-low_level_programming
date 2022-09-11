94ffd6f5cf086248140c3205e4aa3e718d130ea7
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-11 10:04:25 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+/**
+*main - prints all number of 16
+*
+*Return: 0
+*
+*/
+
+
+int main(void)
+
+{
+
+	int num;
+	char letter;
+
+	for (num = 0; num <= 10; num++)
+	{
+	putchar((num % 10) + '0');
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+
+	{
+
+	putchar(letter);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+	}
