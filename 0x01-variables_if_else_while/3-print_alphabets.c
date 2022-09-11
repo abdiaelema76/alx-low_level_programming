b8620bcd48475ba3179f7c85f13450df5e15074e
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-11 07:42:41 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,13 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
 	putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
+	}
 
 	putchar('\n');
 	return (0);
