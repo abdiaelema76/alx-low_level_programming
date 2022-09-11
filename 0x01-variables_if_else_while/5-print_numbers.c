df7b85663adc8a10e9e44a92a5a16063c5007f28
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-11 08:10:52 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+*main - Entry point
+*
+*Description: Prints all single digit of numbers
+*
+*base 10 from 0
+*
+*Return: 0
+*
+*/
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		pritf("%d", i);
+		}
+
+		putchar('\n');
+		return (0);
+		}
