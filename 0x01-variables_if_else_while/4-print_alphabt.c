8d6e37e3e12bf0e71c80fd382769160228a8c3ab
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-11 08:01:29 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - prints all lower case letters except q and e
+*
+*Return: 0
+*
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter == 'q' || letter == 'e')
+
+	continue;
+	else
+	putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+	}
