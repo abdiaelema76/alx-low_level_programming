1d371efea4317cda240fd45c5f464038aa31fcf0
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-12 12:19:30 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <sttdio.h>
+
+/**
+*main - prints programme that executes different commbinations of twoo digit
+*
+*Return: 0
+*
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i);
+	}
+	for (j = 0; j <= 9; j++)
+	{
+	putchar(j);
+	}
+	putchar(',');
+
+	putchar(" ");
+
+	putchar('\n');
+
+	return (0);
+
+	}
