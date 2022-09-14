ac5624a7d66b9726266e39f961aa4ab3a0f845ac
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-14 11:44:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
-#include "main.h"
-#include  "_putchar.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 *main - prints output
 *
@@ -10,14 +11,7 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar(_putchar);
 	_putchar('\n');
 
 	return (0);
