2ad8f58689aa9f64c622ef48efd72d54ef977273
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-14 00:47:52 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main - prints output
+*
+*Return: Always 0
+*
+*/
+
+int main(void)
+{
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u')'
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	return (0);
+
+	}
