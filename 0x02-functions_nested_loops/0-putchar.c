91fe01d6d092a7c39267bf13299e328da7c501e5
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-14 12:02:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - prints output
+*main - Entry point
 *
 *Return: Always 0
 *
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	printf('_putchar\n');
+	printf("_putchar\n");
 
 	return (0);
 
