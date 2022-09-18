d65748d30eeadc44b5103324fa58b50f67b780a3
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-18 12:43:42 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 /**
 *_isupper - checks is acharacter is uppercase
 *@c: is the int used for the argument of the function.
-*Return: 1if uppercase and 0 if not
+*Return: 1 if uppercase and 0 if not
 */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 
-	return (1);
+		return (1);
 
 	else
-	return (0);
+		return (0);
