bbd9278071563f2683df781996bada4c57297258
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-17 23:28:08 -0700

    Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,11 @@
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+	void positive_or_negetive(int i);
+	int largest_number(int, int, int);
+	void print_remaining_three_days(int, int, int);
+	int convert_day(int, int);
+
+#endif /*MAIN_H*/
