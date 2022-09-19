231b51aed71125a2e6d56f2f3b288164dd98624b
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-19 13:59:43 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*reset_to_98 - resets the value of the variable.
+*pointer is pointing to 98
+*@n: argument to the function
+*Description: print required result
+*Return: return void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	}
