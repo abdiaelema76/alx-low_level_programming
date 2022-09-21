e870d6e96d9deb35c9eb377c256089c6801422ce
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-21 00:46:25 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+*mul - multiplies two intergers.
+*@a: first integer to multiply
+*@b: second interger to multiply
+*return: product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a*b);
+
+	}
