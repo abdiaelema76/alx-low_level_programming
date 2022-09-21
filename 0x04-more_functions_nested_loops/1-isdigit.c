c27808b8aa7ed4bf72945eef161928fe49dc5dde
Abdia Elema <gababoabdiaelema@gmail.com>
2022-09-21 00:56:29 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+/**
+*_isdigit.c - checks for digits
+*@c: character to check
+*Return: 1if c is a digit, otherwise return 0
+*/
+int _isdigit.c(int c)
+{
+	if (c >= '0' && <= '9')
+
+	return (1);
+
+	else
+		return (0);
+		}
