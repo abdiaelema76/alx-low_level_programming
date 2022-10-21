60f3ef00618aef55611687f4e438d182af55acb7
Abdia Elema <gababoabdiaelema@gmail.com>
2022-10-20 22:45:08 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * * print_line - prints line in the terminal
+ * * @n: number of times line is printed
+ * * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			if (i == n)
+			{
+				_putchar('_');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('_');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
